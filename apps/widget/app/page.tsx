f1370e89b9dafb6434920542414054ff731fbb84
sardormahmudovbellissimo <sardor.mahmudov@bellissimo.uz>
2025-11-19 23:24:14 +0500

    mono repo created

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,12 +1,10 @@
 import { Button } from "@workspace/ui/components/button"
-import { add } from "@workspace/math/add"
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello /</h1>
-        <h1 className="text-2xl font-bold">Hello {add(5, 2)}</h1>
+        <h1 className="text-2xl font-bold">Hello World /</h1>
         <Button size="sm">Button</Button>
       </div>
     </div>
